f8c065c588cdfedfc6d04714b5739b88e62b8941
philipperemy <premy@reactive.co.jp>
2017-06-01 18:10:40 +0900

    tf 1.0 and python 3 support

---
+++ train.py
@@ -0,0 +1,102 @@
+import argparse
+import os
+import pickle
+import time
+
+import pandas as pd
+import tensorflow as tf
+
+from model import Model
+from utils import TextLoader
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--data_dir', type=str, default='data/tinyshakespeare',
+                        help='data directory containing input.txt')
+    parser.add_argument('--save_dir', type=str, default='save',
+                        help='directory to store checkpointed models')
+    parser.add_argument('--rnn_size', type=int, default=128,
+                        help='size of RNN hidden state')
+    parser.add_argument('--num_layers', type=int, default=2,
+                        help='number of layers in the RNN')
+    parser.add_argument('--model', type=str, default='lstm',
+                        help='rnn, gru, lstm, gridlstm, gridgru')
+    parser.add_argument('--batch_size', type=int, default=50,
+                        help='minibatch size')
+    parser.add_argument('--seq_length', type=int, default=50,
+                        help='RNN sequence length')
+    parser.add_argument('--num_epochs', type=int, default=50,
+                        help='number of epochs')
+    parser.add_argument('--save_every', type=int, default=1000,
+                        help='save frequency')
+    parser.add_argument('--grad_clip', type=float, default=5.,
+                        help='clip gradients at this value')
+    parser.add_argument('--learning_rate', type=float, default=0.002,
+                        help='learning rate')
+    parser.add_argument('--decay_rate', type=float, default=0.97,
+                        help='decay rate for rmsprop')
+    args = parser.parse_args()
+    print(args)
+    train(args)
+
+
+def train(args):
+    data_loader = TextLoader(args.data_dir, args.batch_size, args.seq_length)
+    args.vocab_size = data_loader.vocab_size
+
+    with open(os.path.join(args.save_dir, 'config.pkl'), 'wb') as f:
+        pickle.dump(args, f)
+    with open(os.path.join(args.save_dir, 'chars_vocab.pkl'), 'wb') as f:
+        pickle.dump((data_loader.chars, data_loader.vocab), f)
+
+    model = Model(args)
+
+    with tf.Session() as sess:
+        tf.initialize_all_variables().run()
+        saver = tf.train.Saver(tf.all_variables())
+        train_loss_iterations = {'iteration': [], 'epoch': [], 'train_loss': [], 'val_loss': []}
+
+        for e in range(args.num_epochs):
+            sess.run(tf.assign(model.lr, args.learning_rate * (args.decay_rate ** e)))
+            data_loader.reset_batch_pointer()
+            state = sess.run(model.initial_state)
+            for b in range(data_loader.num_batches):
+                start = time.time()
+                x, y = data_loader.next_batch()
+                feed = {model.input_data: x, model.targets: y, model.initial_state: state}
+                train_loss, state, _ = sess.run([model.cost, model.final_state, model.train_op], feed)
+                end = time.time()
+                batch_idx = e * data_loader.num_batches + b
+                print("{}/{} (epoch {}), train_loss = {:.3f}, time/batch = {:.3f}" \
+                      .format(batch_idx,
+                              args.num_epochs * data_loader.num_batches,
+                              e, train_loss, end - start))
+                train_loss_iterations['iteration'].append(batch_idx)
+                train_loss_iterations['epoch'].append(e)
+                train_loss_iterations['train_loss'].append(train_loss)
+
+                if batch_idx % args.save_every == 0:
+
+                    # evaluate
+                    state_val = sess.run(model.initial_state)
+                    avg_val_loss = 0
+                    for x_val, y_val in data_loader.val_batches:
+                        feed_val = {model.input_data: x_val, model.targets: y_val, model.initial_state: state_val}
+                        val_loss, state_val, _ = sess.run([model.cost, model.final_state, model.train_op], feed_val)
+                        avg_val_loss += val_loss / len(list(data_loader.val_batches))
+                    print('val_loss: {:.3f}'.format(avg_val_loss))
+                    train_loss_iterations['val_loss'].append(avg_val_loss)
+
+                    checkpoint_path = os.path.join(args.save_dir, 'model.ckpt')
+                    saver.save(sess, checkpoint_path, global_step=e * data_loader.num_batches + b)
+                    print("model saved to {}".format(checkpoint_path))
+                else:
+                    train_loss_iterations['val_loss'].append(None)
+
+            pd.DataFrame(data=train_loss_iterations,
+                         columns=train_loss_iterations.keys()).to_csv(os.path.join(args.save_dir, 'log.csv'))
+
+
+if __name__ == '__main__':
+    main()
---
+++ utils.py
@@ -0,0 +1,106 @@
+import codecs
+import collections
+import os
+import pickle
+
+import numpy as np
+
+
+class TextLoader(object):
+    def __init__(self, data_dir, batch_size, seq_length):
+        self.data_dir = data_dir
+        self.batch_size = batch_size
+        self.seq_length = seq_length
+
+        input_file = os.path.join(data_dir, "input.txt")
+        vocab_file = os.path.join(data_dir, "vocab.pkl")
+        tensor_file = os.path.join(data_dir, "data.npy")
+
+        if not (os.path.exists(vocab_file) and os.path.exists(tensor_file)):
+            print("reading text file")
+            self.preprocess(input_file, vocab_file, tensor_file)
+        else:
+            print("loading preprocessed files")
+            self.load_preprocessed(vocab_file, tensor_file)
+        self.create_batches()
+        self.reset_batch_pointer()
+
+    def preprocess(self, input_file, vocab_file, tensor_file):
+        with codecs.open(input_file, "r") as f:
+            data = f.read()
+        counter = collections.Counter(data)
+        count_pairs = sorted(counter.items(), key=lambda x: -x[1])
+        self.chars, _ = list(zip(*count_pairs))
+        self.vocab_size = len(self.chars)
+        self.vocab = dict(zip(self.chars, range(len(self.chars))))
+        with open(vocab_file, 'wb') as f:
+            pickle.dump(self.chars, f)
+        self.tensor = np.array(list(map(self.vocab.get, data)))
+        np.save(tensor_file, self.tensor)
+
+    def load_preprocessed(self, vocab_file, tensor_file):
+        with open(vocab_file, 'rb') as f:
+            self.chars = pickle.load(f)
+        self.vocab_size = len(self.chars)
+        self.vocab = dict(zip(self.chars, range(len(self.chars))))
+        self.tensor = np.load(tensor_file)
+        self.num_batches = self.tensor.size // (self.batch_size * self.seq_length)
+
+    def create_batches(self):
+        self.num_batches = self.tensor.size // (self.batch_size * self.seq_length)
+        self.tensor = self.tensor[:self.num_batches * self.batch_size * self.seq_length]
+        xdata = self.tensor
+        ydata = np.copy(self.tensor)
+        ydata[:-1] = xdata[1:]
+        ydata[-1] = xdata[0]
+        self.x_batches = np.split(xdata.reshape(self.batch_size, -1), self.num_batches, 1)
+        self.y_batches = np.split(ydata.reshape(self.batch_size, -1), self.num_batches, 1)
+
+        validation_batches = int(self.num_batches * .2)
+        self.val_batches = zip(self.x_batches[-validation_batches:], self.y_batches[-validation_batches:])
+        self.x_batches = self.x_batches[:-validation_batches]
+        self.y_batches = self.y_batches[:-validation_batches]
+        self.num_batches -= validation_batches
+
+    def next_batch(self):
+        x, y = self.x_batches[self.pointer], self.y_batches[self.pointer]
+        self.pointer += 1
+        return x, y
+
+    def reset_batch_pointer(self):
+        self.pointer = 0
+
+
+def visualize_result():
+    import pandas as pd
+    import matplotlib.pyplot as plt
+
+    # These are the "Tableau 20" colors as RGB.
+    tableau20 = [(31, 119, 180), (174, 199, 232), (255, 127, 14), (255, 187, 120),
+                 (44, 160, 44), (152, 223, 138), (214, 39, 40), (255, 152, 150),
+                 (148, 103, 189), (197, 176, 213), (140, 86, 75), (196, 156, 148),
+                 (227, 119, 194), (247, 182, 210), (127, 127, 127), (199, 199, 199),
+                 (188, 189, 34), (219, 219, 141), (23, 190, 207), (158, 218, 229)]
+
+    # Scale the RGB values to the [0, 1] range, which is the format matplotlib accepts.
+    for i in range(len(tableau20)):
+        r, g, b = tableau20[i]
+        tableau20[i] = (r / 255., g / 255., b / 255.)
+
+    files = [('GridGRU, 3 layers', 'save_gridgru3layers/log.csv'),
+             # ('GridGRU, 6 layers', 'save_gridgru6layers/log.csv'),
+             ('GridLSTM, 3 layers', 'save_gridlstm3layers/log.csv'),
+             ('GridLSTM, 6 layers', 'save_gridlstm6layers/log.csv'),
+             ('Stacked GRU, 3 layers', 'save_gru3layers/log.csv'),
+             # ('Stacked GRU, 6 layers', 'save_gru6layers/log.csv'),
+             ('Stacked LSTM, 3 layers', 'save_lstm3layers/log.csv'),
+             ('Stacked LSTM, 6 layers', 'save_lstm6layers/log.csv'),
+             ('Stacked RNN, 3 layers', 'save_rnn3layers/log.csv'),
+             ('Stacked RNN, 6 layers', 'save_rnn6layers/log.csv')]
+    for i, (k, v) in enumerate(files):
+        train_loss = pd.read_csv('./save/tinyshakespeare/{}'.format(v)).groupby('epoch').mean()['train_loss']
+        plt.plot(train_loss.index.tolist(), train_loss.tolist(), label=k, lw=2, color=tableau20[i * 2])
+    plt.legend()
+    plt.xlabel('Epochs')
+    plt.ylabel('Average training loss')
+    plt.show()
